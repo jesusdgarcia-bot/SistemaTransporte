ab5cf341c56c776a6e6962fa38709fe59c530e6c
Abisael Freyle Almenarez <afreylea@unicesar.edu.co>
2026-03-14 11:25:42 -0500

    feat(interfaces): crear interfaces Imprimible y Calculable

---
+++ src/sistematransporte/model/interfaces/Calculable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package sistematransporte.model.interfaces;
+
+/**
+ *
+ * @author abisa
+ */
+public interface Calculable {
+    public double calcularTotal();
+}
