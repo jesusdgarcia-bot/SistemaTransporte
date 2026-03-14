69c54245c908f46258eda38e774b7b9bb6965555
Abisael Freyle Almenarez <afreylea@unicesar.edu.co>
2026-03-14 12:17:07 -0500

    feat(model): crear clase abstracta Vehiculo con atributos

--- src/sistematransporte/model/Vehiculo.java
+++ src/sistematransporte/model/Vehiculo.java
@@ -1,9 +1,16 @@
 package sistematransporte.model;
-
+import sistematransporte.model.interfaces.*;
 /**
  *
  * @author jssdv
  */
-public class Vehiculo {
-    
+abstract class Vehiculo implements Imprimible, Calculable {
+  private String placa;
+  private String ruta;
+  private double tarifa;
+  private int capacidadMaxima; 
+  private int pasajerosActuales;
+  private Boolean estado;
+  private String idLicenciaConductor;
+  private int ticketsVendidos;
 }
