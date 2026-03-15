16ef30430ee9ca4fa700de7ecaa802370c130de6
Abisael Freyle <afreylea@unicesar.edu.co>
2026-03-15 14:39:19 -0500

    feat(service): inicializar constructor y atributos para clase VehiculoService

--- src/sistematransporte/service/VehiculoService.java
+++ src/sistematransporte/service/VehiculoService.java
@@ -1,9 +1,28 @@
 package sistematransporte.service;
 
+import sistematransporte.dao.VehiculoDao;
+import sistematransporte.dao.ConductorDao;
+import sistematransporte.model.Conductor;
+import sistematransporte.model.Vehiculo;
+import sistematransporte.model.Bus;
+import sistematransporte.model.Microbus;
+import sistematransporte.model.Buseta;
+
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author jssdv
  */
+
 public class VehiculoService {
-    
+
+  private VehiculoDao vehiculoDao;
+  private ConductorDao conductorDao;
+
+  public VehiculoService(){
+    this.vehiculoDao = new VehiculoDao();
+    this.conductorDao = new ConductorDao();
+  }  
 }
