f01911852c07c7241edb9ef2419a8f9f65460e0b
Abisael Freyle <afreylea@unicesar.edu.co>
2026-03-15 12:39:00 -0500

    feat(dao): crear estructura base de VehiculoDao

--- src/sistematransporte/dao/VehiculoDAO.java
+++ src/sistematransporte/dao/VehiculoDAO.java
@@ -1,9 +1,27 @@
 package sistematransporte.dao;
 
+import sistematransporte.model.Vehiculo;
+import sistematransporte.model.Bus;
+import sistematransporte.model.Microbus;
+import sistematransporte.model.Buseta;
+import sistematransporte.model.Conductor;
+import sistematransporte.dao.ConductorDao;
+import sistematransporte.util.RutaArchivos;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author jssdv
  */
-public class VehiculoDAO {
-    
+
+public class VehiculoDao {
+
+    private ConductorDao conductorDao;
+
+    public VehiculoDao(){
+        this.conductorDao = new ConductorDao();
+    }   
 }
