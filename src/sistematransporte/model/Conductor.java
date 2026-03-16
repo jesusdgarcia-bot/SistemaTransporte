bc1f2639210b730b7a5267ef70e06d287b652e6d
Jesus David <jdaviddominguez@unicesar.edu.co>
2026-03-15 19:24:32 -0500

    feat: crear Conductor con numeroLicencia y metodo tieneLicencia

--- src/sistematransporte/model/Conductor.java
+++ src/sistematransporte/model/Conductor.java
@@ -4,14 +4,47 @@
  */
 package sistematransporte.model;
 
+import sistematransporte.model.interfaces.Imprimible;
+
 /**
  *
  * @author almen
  */
-public class Conductor {
+public class Conductor extends Persona implements Imprimible {
+    private String numeroLicencia;
+    private String categoriaLicencia; // B1, B2, C1, C2
     private boolean disponible;
+ 
+    public Conductor(String cedula, String nombre, String numeroLicencia, String categoriaLicencia) {
+        super(cedula, nombre);
+        this.numeroLicencia = numeroLicencia;
+        this.categoriaLicencia = categoriaLicencia;
+    }
+ 
+    public String getNumeroLicencia() {
+        return numeroLicencia;
+    }
+ 
+    public void setNumeroLicencia(String numeroLicencia) {
+        this.numeroLicencia = numeroLicencia;
+    }
+ 
+    public String getCategoriaLicencia() {
+        return categoriaLicencia;
+    }
+ 
+    public void setCategoriaLicencia(String categoriaLicencia) {
+        this.categoriaLicencia = categoriaLicencia;
+    }
+ 
+    // Retorna true si el conductor tiene licencia valida
+    public boolean tieneLicencia() {
+        return numeroLicencia != null && !numeroLicencia.trim().isEmpty();
+    }
+    ;
     
     public boolean getDisponible(){
         return disponible;
     }
+    
 }
